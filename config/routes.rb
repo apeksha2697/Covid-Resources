56be76dbffbdcb61ab8fb74e2b0bc2ad966ff8f9
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-04 18:26:26 +0530

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cities, only: [:index, :create] do
+    get :search, on: :collection
+  end
+  resources :resources, only: [:index, :create]
+  resources :tweets, only: [:create] do
+    get :search, on: :collection
+  end
+  
+  root  'tweets#dashboard'
 end
