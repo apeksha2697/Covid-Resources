b352f89e23d08911592aa98bc63d8ef8fea1bbf8
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-04 18:27:27 +0530

    added tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_074706) do
+ActiveRecord::Schema.define(version: 2021_05_04_112356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_05_04_074706) do
 
   create_table "resources", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "city", null: false
+    t.string "resource", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
