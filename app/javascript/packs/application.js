dd040b3297042ca8c40d11a638b5363db29f8afa
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-05 16:11:40 +0530

    removed redunant code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/custom")
-require("packs/post.js")
 import "@fortawesome/fontawesome-free/css/all"
 
 
