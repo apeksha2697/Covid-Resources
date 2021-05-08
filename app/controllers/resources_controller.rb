99d9232937cb23d6bf74d999518f9d3de69ad27f
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-08 14:47:42 +0530

    added search method for tweet

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,11 +1,30 @@
 class ResourcesController < ApplicationController
 
   def index
-    Resource.all
+    @resources = Resource.all
   end
 
   def create
     resource = Resource.new(resource_params)
+  end
+
+  def search
+    @tweets = []
+    city = City.find(params[:city_id]).name
+    resource = params[:resource]
+    tweets = Twitter::REST::Client.new do |config|
+      config.consumer_key =  ENV['TWITTER_API_KEY']
+      config.consumer_secret = ENV['TWITTER_API_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    end
+    @search = tweets.search("(#{city} OR #{'#'+city})  (#{'#'+resource} OR #{'#'+resource+'s'} OR #{resource}) ('#verified' OR 'verified')",  result_type: "recent", tweet_mode: "extended").take(100)
+    @search.each do |tweet|
+      if tweet.created_at > DateTime.now.yesterday.utc
+        @tweets.append(tweet)
+      end
+    end
+    @tweets
   end
 
   private
