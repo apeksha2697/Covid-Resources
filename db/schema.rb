11afcbf4f990d168b9471d0bdf75771009d24fd6
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-08 14:55:56 +0530

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_112356) do
+ActiveRecord::Schema.define(version: 2021_05_06_082605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,15 @@ ActiveRecord::Schema.define(version: 2021_05_04_112356) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "resources", force: :cascade do |t|
-    t.string "name", null: false
+  create_table "city_resources", id: false, force: :cascade do |t|
+    t.integer "city_id"
+    t.integer "resource_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tweets", force: :cascade do |t|
-    t.string "city", null: false
-    t.string "resource", null: false
+  create_table "resources", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
