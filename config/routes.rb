43d06a1bd7a5421f017d03665efa05191c7e14d6
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-08 14:54:51 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :cities, only: [:index, :create] do
     get :search, on: :collection
-  end
-  resources :resources, only: [:index, :create]
-  resources :tweets, only: [:create] do
-    get :search, on: :collection
+    resources :resources, only: [:index, :create] do
+      get :search, on: :collection
+    end
   end
   
-  root  'tweets#dashboard'
+  root  'cities#index'
 end
