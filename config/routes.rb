6cca754009888afe018a98bcb341c36453ef94b1
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-12 18:00:18 +0530

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resource :vaccines do
     get :state, on: :collection
+    get :district, on: :collection
+    get :find_by_district, on: :collection
   end
   
   root  'resources#index'
