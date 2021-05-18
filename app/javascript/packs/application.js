54546d3ff9bf6658aee634213eb3fccbca7567e5
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-18 12:53:33 +0530

    changed fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-import "@fortawesome/fontawesome-free/js/all";
-import "@fortawesome/fontawesome-free/css/all";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
